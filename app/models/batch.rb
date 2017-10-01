b72d3cca8d37fc0fb0dabb69a129be3e91d52ac5
ssoonsl <ssoonsl616+gh@gmail.com>
2017-10-01 20:57:51 +0800

    Remove previous day plan view

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -3,6 +3,6 @@ class Batch < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :start_date, presence: true
 
-  has_many :weeks, dependent: :destroy
+  has_many :week_plans, dependent: :destroy
 
 end
--- app/models/day_plan.rb
+++ app/models/day_plan.rb
@@ -2,7 +2,7 @@ class DayPlan < ApplicationRecord
 
   validates :day, presence: true
 
-  belongs_to :week
+  belongs_to :week_plan
   has_many :batch_objectives
 
 end
