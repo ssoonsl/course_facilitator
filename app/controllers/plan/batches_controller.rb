b69325d7aa01e135bee7b0b94403e1603a7dd2bf
ssoonsl <ssoonsl616+gh@gmail.com>
2017-10-09 06:45:09 +0800

    Add batch dashboard Add pane to display unadded learning objectives Display learning objectives added to each week

--- app/controllers/plan/batches_controller.rb
+++ app/controllers/plan/batches_controller.rb
@@ -40,6 +40,13 @@ class Plan::BatchesController < ApplicationController
     redirect_to plan_batches_path
   end
 
+  def dashboard
+    @batch = Batch.find(params[:id])
+    @learning_objectives = LearningObjective.all
+    @batch_objective = BatchObjective.new
+    @week_plans = @batch.week_plans.order(week: :asc)
+  end
+
   private
 
   def batch_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'plan/batches#index'
 
   namespace :plan do
+    get 'batch_dashboard/:id', to: 'batches#dashboard', as: 'batch_dashboard'
     resources :batches, except: [:show]
     resources :learning_objectives, except: [:show]
+    resources :batch_objectives, only: [:create]
   end
 
 end
